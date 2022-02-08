5c029414492bb7a6132b158e8f74c82caec72f3c
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-08 22:42:04 +0200

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,3 @@
 import { defineApp } from "iles";
 import "elektro/style.css";
-import { initMessages, config } from "elektro";
-
-export default defineApp({
-  enhanceApp() {
-    config.wsUrl = import.meta.env.VITE_WS_URL | "";
-    config.messagesUrl = import.meta.env.VITE_MESSAGES_URL || "";
-    initMessages();
-  },
-});
+export default defineApp({});
