3898022e35bc049e1449df20199d2cb6297cd964
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-08 20:44:20 +0200

    config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
-import "elektro/style.css";
 import { defineApp } from "iles";
+import "elektro/style.css";
+import { initMessages, config } from "elektro";
 
-export default defineApp({});
+export default defineApp({
+  enhanceApp() {
+    config.wsUrl = import.meta.env.VITE_WS_URL | "";
+    config.messagesUrl = import.meta.env.VITE_MESSAGES_URL || "";
+    initMessages();
+  },
+});
