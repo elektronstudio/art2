017d060a305c3e24679f1b8836737fd2cd83ee33
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-09 07:09:10 +0200

    rollback

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="iles" />
-
-interface ImportMetaEnv {
-  readonly VITE_WS_URL: string;
-  readonly VITE_MESSAGES_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
