7af87dc175f9b52955359a1193dc2370464fb3cf
sven-ra <mail@svenraju.net>
2022-02-17 10:27:54 +0200

    feat: add description for index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <ETitle size="lg">Index</ETitle>
+    <EContent size="lg">
+      <p>
+        Transdistsiplinaarne etendustkunsti platvorm, mis liidab kaasaegse
+        kunsti, teaduse ja tehnoloogia otsingulisi tegevusi.
+      </p>
+    </EContent>
   </div>
 </template>
+
+<style scoped>
+.EContent {
+  max-width: 60ch;
+  color: var(--gray-300);
+}
+</style>
