724978d9ade0c74769a19112d74528a8fb4d9d76
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-17 15:20:20 +0200

    cleanup

--- src/components/Fienta.vue
+++ src/components/Fienta.vue
@@ -9,42 +9,63 @@ import {
   getTicketableStatus,
 } from "elektro";
 
-const code = ref("EI9BBMTEGE");
-const ticketable = ref<Ticketable | null>(null);
+const url = new URLSearchParams(window.location.search);
+const urlCode = url.get("code");
+
+const redirect = (code: string) =>
+  (window.location.href = `https://live.elektron.art/fienta?code=${code}`);
+
+if (urlCode) {
+  redirect(urlCode);
+}
+
+const code = ref(urlCode || "");
 
 const onSubmit = () => {
   if (code.value) {
-    validateTicket(code.value).then((t) => {
-      ticketable.value = t;
-    });
+    redirect(code.value);
   }
 };
-
-const ticketableStatus = computed(() => {
-  if (ticketable.value) {
-    const ticketables: Ticketable[] = [
-      ticketable.value,
-      ticketable.value.festival,
-    ];
-    return getTicketableStatus(ticketables);
-  }
-});
 </script>
 
 <template>
   <div class="Fienta">
-    <ETitle size="lg">Strapi</ETitle>
-    <EFormText v-model="code" label="Ticket code" />
-    <EButton @click="onSubmit">Submit</EButton>
-    <pre>{{ ticketableStatus }}</pre>
-    <pre>{{ ticketable }}</pre>
+    <div>
+      <ETitle size="lg">Validate your ticket</ETitle>
+      <EContent>
+        There is a <b>ticket code</b> in your Fienta email, just below the
+        "Sisene üritusele / Enter event" blue button. Copy-paste it to the box
+        below:
+      </EContent>
+      <EFormText v-model="code" label="Ticket code" />
+      <EButton color="accent" size="sm" @click="onSubmit"
+        >Submit ticket code</EButton
+      >
+    </div>
   </div>
 </template>
 
 <style scoped>
 .Fienta {
   display: grid;
+  justify-content: center;
+  align-content: center;
+  min-height: 80vh;
+}
+.Fienta > div {
+  width: 50vw;
+  display: grid;
   gap: var(--gap-4);
-  padding: var(--p-4);
+}
+.Fienta .EButton {
+  justify-self: flex-start;
+}
+@media only screen and (max-width: 600px) {
+  .Fienta {
+    align-content: flex-start;
+  }
+  .Fienta > div {
+    width: 100%;
+  }
 }
 </style>
